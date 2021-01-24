a80807c187f82d6808003616c367a2c9adf53ee6
Gleb <maksimenkogleb@gmail.com>
2021-01-24 16:05:59 +0200

    Add project files.

---
+++ TestApp/Data/PersonContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApp.Models;
+
+namespace TestApp.Data
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ TestApp/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
+
+namespace TestApp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public bool IsMarried { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
